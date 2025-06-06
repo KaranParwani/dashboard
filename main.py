b6b00c03cf126618e1ac4dce17f747d7c0a61249
karanparwani <karan.p@universalassets.com>
2025-06-06 14:29:35 +0400

    new routes and startup event

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import uvicorn
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
 from jose import JWTError
+from fastapi import FastAPI
 from starlette import status
-from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from starlette.middleware.cors import CORSMiddleware
+
+from patient.routes.admin import admin_router
+from patient.routes.chatbot import openai_router
+from patient.routes.patient import patient_router
 
 from config import HOST, PORT
+from patient.services.admin import add_super_admin
+from patient.services.database import db_manager
 
 app = FastAPI(title="Patient Assistant API")
+
+
+@app.on_event("startup")
+def startup_event():
+    """Run database initialization and super admin creation at startup."""
+    db_manager.connect()
+    add_super_admin()
 
 
 @app.exception_handler(JWTError)
@@ -25,7 +38,7 @@ async def handle_invalid_header_error(request, exc) -> JSONResponse:
         status_code=422,
         content={
             "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
-            "message": "Invalid header. Please check your JWT token.",
+            "message": "Invalid header. Please chec k your JWT token.",
         },
     )
 
@@ -72,6 +85,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(patient_router, prefix="/patients", tags=["Patient"])
+app.include_router(admin_router, prefix="/admin", tags=["Admin"])
+app.include_router(openai_router, prefix="/openai", tags=["Open AI"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST, port=int(PORT))
