c9ce864ffed93590cf4d8e2db8ce061e442bf530
karanparwani <karan.p@universalassets.com>
2025-06-07 21:31:08 +0400

    added get patient details by id

--- patient/routes/patient.py
+++ patient/routes/patient.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends, Header, HTTPException
-from fastapi.security import HTTPAuthorizationCredentials
+from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
 from starlette import status
 
 from config import SECRET_KEY, ALGORITHM
-from patient.schemas.patient import PatientRecord
+from patient.schemas.patient import PatientRecord, UpdatePatientRecord
 from patient.services.admin import AdminAuthenticator
 from patient.services.database import db_manager
 from patient.services.exceptions import AuthJWTException, response_structure
@@ -15,15 +14,20 @@ patient_router = APIRouter()
 
 @patient_router.get("/")
 async def get_patient_details(
+    patient_id: int = None,
     session: Session = Depends(db_manager.get_database_session),
     authorization: str = Header(None),
 ):
     try:
         authenticate = AdminAuthenticator(session, SECRET_KEY, ALGORITHM)
-        print(authenticate.authenticate_admin_token(authorization))
+        authenticate.authenticate_admin_token(authorization)
 
         patients = PatientsManager(session)
-        return await patients.get_patients_details()
+
+        if patient_id is not None:
+            return await patients.get_patients_details(patient_id)
+        else:
+            return await patients.get_patients_details()
 
     except Exception as e:
         if isinstance(e, AuthJWTException):
@@ -58,7 +62,7 @@ async def add_patient_details(
 
 @patient_router.put("/update")
 async def update_patient_details(
-    patient_details: PatientRecord,
+    patient_details: UpdatePatientRecord,
     session: Session = Depends(db_manager.get_database_session),
     authorization: str = Header(None),
 ):
@@ -68,7 +72,7 @@ async def update_patient_details(
         admin_details = authenticate.authenticate_admin_token(authorization)
 
         patients = PatientsManager(session, patient_details.model_dump())
-        return await patients.update_patients_details(admin_details.get('user_id'))
+        return await patients.update_patients_details(admin_details.get("user_id"))
 
     except Exception as e:
         if isinstance(e, AuthJWTException):
@@ -78,15 +82,19 @@ async def update_patient_details(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message=str(e), data=None
         )
 
+
 @patient_router.delete("/delete")
-async def delete_patient_record(patient_id: int = None, admin_id: int = None, session: Session = Depends(db_manager.get_database_session),
-    authorization: str = Header(None)):
+async def delete_patient_record(
+    patient_id: int = None,
+    session: Session = Depends(db_manager.get_database_session),
+    authorization: str = Header(None),
+):
     try:
         authenticate = AdminAuthenticator(session, SECRET_KEY, ALGORITHM)
-        authenticate.authenticate_admin_token(authorization)
+        admin = authenticate.authenticate_admin_token(authorization)
 
         patients = PatientsManager(session)
-        return await patients.delete_patient_record(patient_id, admin_id)
+        return await patients.delete_patient_record(patient_id, admin["user_id"])
 
     except Exception as e:
         if isinstance(e, AuthJWTException):
